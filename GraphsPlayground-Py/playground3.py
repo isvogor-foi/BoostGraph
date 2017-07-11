897fec8ba68d7d4764e307f8e2b55542d3d0a0b2
isvogor <isvogor@gmail.com>
2017-07-11 17:18:03 -0400

    nongreedy trees

--- GraphsPlayground-Py/playground3.py
+++ GraphsPlayground-Py/playground3.py
@@ -46,10 +46,11 @@ class TreeNode:
         return my_name
 
 class Graphs:
-    points = np.random.random((30, 2)) * 4
-    g, pos = triangulation(points, type="delaunay")
+    #points = np.random.random((30, 2)) * 4
+    #g, pos = triangulation(points, type="delaunay")
     # g.save("graph-output.xml")
-    #g = load_graph("temp.xml")
+    g = load_graph("temp.xml")
+    #g = load_graph("/home/ivan/Downloads/temp-graph.xml")
     pos = fruchterman_reingold_layout(g)
     weight = g.new_edge_property("double")
     #g.save("temp.xml")
@@ -58,7 +59,7 @@ class Graphs:
         b = closeness(self.g)
         print("Res: ", b.ma)
         b = closeness(self.g)
-        graph_draw(self.g, pos=self.pos, output_size=(600, 600), vertex_text=self.g.vertex_index, vertex_fill_color=b, output=output)
+        graph_draw(self.g, pos=self.pos, output_size=(800, 600), vertex_text=self.g.vertex_index, vertex_fill_color=b, output=output)
 
     def betweenness_graph(self):
         bv, be = betweenness(self.g)
@@ -102,20 +103,6 @@ class Graphs:
 
     def new_thing(self):
         c = closeness(self.g)
-        my_var = c.ma
-        print("My var", type(my_var))
-        my_nodes = []
-
-        counter = 0
-        for node in np.nditer(my_var):
-            my_nodes.append(MyNodes(counter, node))
-            counter = counter + 1
-
-        print("Vertex: ", type(self.g.vertices()))
-        print("Vertex: ", np.isin(1, self.g.get_vertices()))
-
-        print("Neighbors: ", self.are_neighbors(self.g, 11, 13))
-        print("Distance: ", self.distance_to(self.g, 10, 13))
 
         #for my_list in sorted(my_nodes, reverse=True):
         #    print("Node: ", my_list.get_node())
@@ -127,84 +114,120 @@ class Graphs:
         #
         # self.construct_tree_from_old(dual_tree, temp_tree, 5, 3)
 
-        dual_tree = self.g.copy()
-        temp_tree = self.g.copy()
+        edgeless_graph_copy = self.g.copy()
         for edge in self.g.edges():
-            dual_tree.remove_edge(edge)
+            edgeless_graph_copy.remove_edge(edge)
 
-        forest = self.construct_tree_balanced(dual_tree, temp_tree, 5, 5)
+        depth = 5
+        no_duplicate_tree = self.remove_duplicates(self.g.copy())
+        forest = self.construct_tree_balanced(no_duplicate_tree, [18, 15], depth)
+        print("Done with forest generation, drawing...")
+        self.draw_mynode_tree(depth, forest, edgeless_graph_copy)
+
+        ## temp
+        #g2 = self.g.copy()
+        #self.remove_duplicates(g2)
+        # end temp
+
         for tree in forest:
-            subgraph = self.get_tree(tree, 5)
-            graph_draw(subgraph, pos=fruchterman_reingold_layout(subgraph), vertex_text=subgraph.vertex_index)
+            subgraph, v_prop = self.get_tree(tree, depth)
+            #self.remove_duplicates(subgraph)
+            graph_draw(subgraph, pos=fruchterman_reingold_layout(subgraph), vertex_text=v_prop)
 
+    def remove_duplicates(self, original_graph):
+        graph_copy = original_graph.copy()
+        graph_copy.clear_edges()
+        taken_list = []
+
+        for graph_edge in original_graph.edges():
+            source = int(graph_edge.source())
+            target = int(graph_edge.target())
+            current = [target, source]
+            current_reversed = [source, target]
+            if current not in taken_list and target in original_graph.vertices():
+                graph_copy.add_edge(graph_edge.source(), graph_edge.target())
+                taken_list.append(current)
+                taken_list.append(current_reversed)
+
+        #graph_draw(graph_copy, pos=self.pos, vertex_text=graph_copy.vertex_index)
+        return graph_copy
 
     # -------------------------------------------------------------------------------
 
     def get_tree(self, branch, max_depth):
-        node_list = list()
-        node_list.append(branch)
-        child_index = 0
+        current_children_list = branch.get_children()
+        subtree_vertices = list()
+        subtree_vertices.append(branch.get_id())
+        current_depth = 0
 
-        # create the list of all children, i.e. vertices in the given branch
-        for depth in range(max_depth):
-            #print("Depth: ", depth, " len: ", len(node_list))
-            if depth < len(node_list):
-                num_children = len(node_list[depth].get_children())
-                #print("Depth: ", depth, " Children: ", num_children)
-                for i in range(num_children):
-                    children = node_list[depth].get_children()
-                    for child in children:
-                        if child not in node_list:
-                            node_list.append(child)
-                    child_index += 1
-
-        # get only IDs
-        tree_ints = [i.get_id() for i in node_list]
+        while current_depth < max_depth:
+            # draw verices
+            next_children_list = []
+            for current_child in current_children_list:
+                subtree_vertices.append(current_child.get_id())
+                for child in current_child.get_children():
+                    next_children_list.append(child)
+            # go to next level depth
+            current_depth += 1
+            current_children_list = next_children_list
+        # while current_depth < max_depth:
+        #     # draw verices
+        #     for current_child in current_children_list:
+        #         all.append(current_child.get_id())
+        #
+        #     # go to next level depth
+        #     current_depth += 1
+        #     next_children_list = []
+        #     for current_child in current_children_list:
+        #         for child in current_child.get_children():
+        #             next_children_list.append(child)
+        #     current_children_list = next_children_list
 
         subgraph = self.g.copy()
+        v_prop = subgraph.new_vertex_property("string")
+
         # create a subgraph by removing nodes not in the tree
         for vertex in reversed(sorted(self.g.get_vertices())):
-            if int(vertex) not in tree_ints:
+            if int(vertex) not in subtree_vertices:
                 subgraph.remove_vertex(int(vertex))
+            else:
+                v_prop[subgraph.vertex(vertex)] = str(vertex)
 
-        return subgraph
+        print("ALL: ", subtree_vertices, " v_prop: ", v_prop)
+        return subgraph, v_prop
 
-    def construct_tree_balanced(self, original_graph, temp_tree, starting_node, depth):
+    def construct_tree_balanced(self, original_graph, roots, depth):
         # --- Experimental
         # 1, 5, 11, 14, 18
         graph_nodes = []
         taken_nodes = []
 
-        graph_nodes.append(TreeNode(29, 0))
-        graph_nodes.append(TreeNode(18, 0))
-        graph_nodes.append(TreeNode(15, 0))
-
-        taken_nodes.append(29)
-        taken_nodes.append(18)
-        taken_nodes.append(15)
+        for root in roots:
+            graph_nodes.append(TreeNode(root, 0))
+            taken_nodes.append(root)
         d = 0
 
-        current_level_set = graph_nodes
-        next_set = []
-        go_next_level = False
+        current_depth_nodes = graph_nodes
+        increase_depth = False
 
         while d < depth:
-            if go_next_level:
+            # if going to next depth level, set the nodes
+            if increase_depth:
                 #print("Next level: ", d)
-                next_set = []
-                for p in current_level_set:
-                    #for c in p.get_children():
-                        #next_set.append(c)
-                    next_set = list(itertools.chain.from_iterable(itertools.zip_longest(next_set, p.get_children())))
-                current_level_set = list(filter(None.__ne__, next_set))
-                #print("Set: ", current_level_set)
-                go_next_level = False
-            if not current_level_set:
+                next_depth_nodes = []
+                for p in current_depth_nodes:
+                    # zip the lists otherwise you are greedy again
+                    next_depth_nodes = list(itertools.chain.from_iterable(
+                        itertools.zip_longest(next_depth_nodes, p.get_children())))
+                current_depth_nodes = list(filter(None.__ne__, next_depth_nodes))
+                increase_depth = False
+            if not current_depth_nodes:
                 break
 
-            empty_set_counter = len(current_level_set)
-            for i in current_level_set:
-                temp = self.get_free_neighbor_new(i, temp_tree, taken_nodes)
+            # add new children, non-greedy
+            empty_set_counter = len(current_depth_nodes)
+            for i in current_depth_nodes:
+                temp = self.get_free_neighbor_new(i, original_graph, taken_nodes)
                 if temp is not -1:
                     taken_nodes.append(temp)
                     new_node = TreeNode(temp, d + 1);
@@ -213,11 +236,13 @@ class Graphs:
                     #print(i, " takes: ", temp)
                 else:
                     empty_set_counter -= 1
-                    if empty_set_counter is 0:  # I fetched empty for each branch, go to next level
+                    # I fetched empty for each branch, go to next level
+                    if empty_set_counter is 0:
                         d += 1
-                        go_next_level = True
-        print("Done with forest generation, drawing...")
+                        increase_depth = True
+        return graph_nodes
 
+    def draw_mynode_tree(self, depth, graph_nodes, original_graph):
         current_set = graph_nodes
         d = 0
         while d < depth:
@@ -234,7 +259,6 @@ class Graphs:
             current_set = next_set
         # draw solution
         graph_draw(original_graph, pos=self.pos, vertex_text=original_graph.vertex_index)
-        return graph_nodes
 
     # -------------------------------------------------------------------------------
 
@@ -250,7 +274,7 @@ class Graphs:
         # --- Experimental
         # 1, 5, 11, 14, 18
         graph1_final = []
-        graph1_final.append([29, 18, 15])
+        graph1_final.append([18, 15])
         for j in range(0, depth):
             graph1 = graph1_final[j]
             for k in graph1:
@@ -331,7 +355,7 @@ class Graphs:
 if __name__ == '__main__':
     graphs = Graphs()
     #graphs.min_spanning_tree()
-    #graphs.closeness_graph()
+    graphs.closeness_graph()
     #graphs.betweenness_graph()
     graphs.new_thing()
     graphs.all_paths()
